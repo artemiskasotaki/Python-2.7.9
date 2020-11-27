d4699123b52f0194be0fd38ba8c0e686ae730550
Artemis <52401191+artemiskasotaki@users.noreply.github.com>
2020-11-27 18:00:58 +0200

    Create Clinic.py.py

---
+++ Clinic/Clinic.py.py
@@ -0,0 +1,33 @@
+A=[]
+Clinic=[]
+
+for i in range(10):
+    x=raw_input("Give Clinic name:")
+    Clinic.append(x)
+
+N=input("Give number of patients:")
+while N<=10 and N>=1000:
+    N=input("Try again")
+
+for i in range(N):
+    x=input("Give clinic number:")
+    A.append(x)
+
+SumOfPatients=[]
+for i in range(10):
+    SumOfPatients.append(0)
+
+for item in A:
+    SumOfPatients[item]+=1
+
+for i in range(10):
+    print SumOfPatients[i],Clinic[i]
+
+maxi=0
+
+for i in range(10):
+    if SumOfPatients[i]>maxi:
+        maxi=SumOfPatients[i]
+        x=i
+
+print maxi, Clinic[x]
