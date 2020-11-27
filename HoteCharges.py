d41183d6f324174bfd9fe930ecf26d0a5df38f78
Artemis <52401191+artemiskasotaki@users.noreply.github.com>
2020-11-27 16:26:44 +0200

    Add files via upload

---
+++ HoteCharges.py
@@ -0,0 +1,71 @@
+<<<<<<< HEAD
+def charge(days,period):
+    charge = 0
+    if period == "LOW":
+        if days >=1 and days <=3:
+            charge = days*40
+        elif days <=7:
+            charge = days*30
+        else:
+            charge = days*25
+    else:
+        if days >=1 and days<=3:
+            charge = days*70
+        elif days<=7:
+            charge=days*55
+        else:
+            charge=days*50
+    return charge
+
+SumH=0
+SumL=0
+
+for i in range(500):
+    days= input("Give days")
+    period=raw_input("Give period of staying")
+    while period!= "LOW" and period!= "HIGH":
+        period = raw_input("Give again")
+    Charge=charge(days,period)
+    if period=="LOW":
+        SumL+=Charge
+    else:
+        SumH+=Charge
+
+print "Sum of High Period", SumH
+print "Sum of Low Period", SumL
+=======
+def charge(days,period):
+    charge = 0
+    if period == "LOW":
+        if days >=1 and days <=3:
+            charge = days*40
+        elif days <=7:
+            charge = days*30
+        else:
+            charge = days*25
+    else:
+        if days >=1 and days<=3:
+            charge = days*70
+        elif days<=7:
+            charge=days*55
+        else:
+            charge=days*50
+    return charge
+
+SumH=0
+SumL=0
+
+for i in range(3):
+    days= input("Give days")
+    period=raw_input("Give period of staying")
+    while period!= "LOW" and period!= "HIGH":
+        period = raw_input("Give again")
+    Charge=charge(days,period)
+    if period=="LOW":
+        SumL+=Charge
+    else:
+        SumH+=Charge
+
+print "Sum of High Period", SumH
+print "Sum of Low Period", SumL
+>>>>>>> bd2c062920481313bf21679440cd8364b6dee89a
